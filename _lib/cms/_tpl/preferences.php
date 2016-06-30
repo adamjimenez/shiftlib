29cf5e13cde65efad8942d6a047bfe3585b35a56
andyiwest <awesteq@gmail.com>
2016-06-30 13:19:44 +0100

    Tweak Preferences page (#1)
    Tweak Preferences page

--- _lib/cms/_tpl/preferences.php
+++ _lib/cms/_tpl/preferences.php
@@ -1,6 +1,8 @@
-<form method="post">
-<input type="hidden" name="save" value="true">
-Password: <input type="password" name="password"><br />
-<br />
-<button type="submit">Save</button>
-</form>
+<div class="col-sm-6">
+	<form method="post">
+		<input type="hidden" name="save" value="true" >
+		<h3>Change Your Password:</h3>
+		<input type="password" name="password" class="form-control"><br />
+		<button type="submit" class="btn btn-default save-btn">Save</button>
+	</form>
+</div>
