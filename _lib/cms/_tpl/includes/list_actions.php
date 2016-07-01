1d347899b8756618ea704d4a3394aee86aca5677
andyiwest <awesteq@gmail.com>
2016-07-01 12:40:50 +0100

    Create/export/delete tweaks
    Converted buttons to bootstrap

--- _lib/cms/_tpl/includes/list_actions.php
+++ _lib/cms/_tpl/includes/list_actions.php
@@ -10,10 +10,10 @@ if( !$qs ){
 <table width="100%" cellspacing="0" cellpadding="3">
 <tr class="table-heading">
 	<td valign="top">
-		<button type="button" onclick="location.href='?option=<?=$this->section;?>&edit=1&<?=$qs;?>'; return false;" style="font-weight:bold;">Create New</button>
+		<button class="btn btn-default" type="button" onclick="location.href='?option=<?=$this->section;?>&edit=1&<?=$qs;?>'; return false;" style="font-weight:bold;">Create New</button>
 
 
-		<button type="submit" onclick="return export_selected();">Export</button>
+		<button class="btn btn-default" type="submit" onclick="return export_selected();">Export</button>
 
 		<!--
 		<? if( $vars['settings'][$this->section]['sms'] ){ ?>
@@ -21,10 +21,10 @@ if( !$qs ){
 		<? } ?>
 		-->
 		<? if( $vars['settings'][$this->section]['shiftmail'] ){ ?>
-		<button type="submit" onclick="return email_selected();">Email</button>
+		<button class="btn btn-default" type="submit" onclick="return email_selected();">Email</button>
 		<? } ?>
 
-		<button type="submit" onclick="return delete_selected();">Delete</button>
+		<button class="btn btn-danger" type="submit" onclick="return delete_selected();">Delete</button>
 	</td>
 	<td align="right" valign="top" style="text-align:right;">
 		<p><?=$p->get_results(1);?></p>
