7043e070001f0afe35abbbdd9df18f7e0a4c033c
andyiwest <awesteq@gmail.com>
2018-07-09 10:54:44 +0100

    update login page layout

--- _lib/cms/_tpl/login.php
+++ _lib/cms/_tpl/login.php
@@ -2,168 +2,194 @@
 if( $auth->user['admin'] ){
 	redirect('/admin');
 }
+$backgrounds = array("https://images.pexels.com/photos/4827/nature-forest-trees-fog.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/247478/pexels-photo-247478.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/556416/pexels-photo-556416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/129105/pexels-photo-129105.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "https://images.pexels.com/photos/833013/pexels-photo-833013.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260,","https://images.pexels.com/photos/462024/pexels-photo-462024.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://images.pexels.com/photos/258112/pexels-photo-258112.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://images.pexels.com/photos/414110/pexels-photo-414110.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+
 ?>
 <style>
-    .header-row {
-      display: none;
-    }
-    .leftcol {
-      display: none;
-    }
-    .content-wrapper {
-        min-height: 900px;
-        z-index: 500;
-        margin-left: 0px;
-        margin-top: 0px;
-    }
-    .form-container {
-        position: absolute;
-        top: 150px;
-        left: 50%;
-        width: 300px;
-        margin-left: -150px;
-        height: 378px;
-        padding: 10px;
-        border-radius: 10px;
-        background: #333;
-    }
-    .form-container-inner {
-        
-        border: 3px solid #fff;
-        width: 294px;
-        float: left;
-        background: #fff;
-        height: 372px;
-        border-radius: 10px;
-    }
-    #content .inner {
-        background: none;
-    }
-    .input-field {
-        float: left;
-        width: 284px;
-        padding: 5px;
-        background: none;
-        border-radius: 10px;
-        border: 1px solid #333;
-    }
-    
-    
-    @import url(https://fonts.googleapis.com/css?family=Roboto:300);
-    
-    .login-page {
-      padding: 8% 0 0;
-      margin: auto;
-    }
-    .form {
-      position: relative;
-      z-index: 1;
-      background: #FFFFFF;
-      max-width: 360px;
-      margin: 0 auto 100px;
-      padding: 45px 45px 25px 45px;
-      text-align: center;
-      box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-    }
-    .form input {
-      font-family: "Roboto", sans-serif;
-      outline: 0;
-      background: #f2f2f2;
-      width: 100%;
-      border: 0;
-      margin: 0 0 15px;
-      padding: 15px;
-      box-sizing: border-box;
-      font-size: 14px;
-    }
-    .form button {
-      font-family: "Roboto", sans-serif;
-      text-transform: uppercase;
-      outline: 0;
-      background: #4CAF50;
-      width: 100%;
-      border: 0;
-      padding: 15px;
-      color: #FFFFFF;
-      font-size: 14px;
-      -webkit-transition: all 0.3 ease;
-      transition: all 0.3 ease;
-      cursor: pointer;
-    }
-    .form button:hover,.form button:active,.form button:focus {
-      background: #43A047;
-    }
-    .form .message {
-      margin: 15px 0 0;
-      color: #b3b3b3;
-      font-size: 12px;
-    }
-    .form .message a {
-      color: #4CAF50;
-      text-decoration: none;
-    }
-    .form .register-form {
-      display: none;
-    }
-    .container {
-      position: relative;
-      z-index: 1;
-      max-width: 300px;
-      margin: 0 auto;
-    }
-    .container:before, .container:after {
-      content: "";
-      display: block;
-      clear: both;
-    }
-    .container .info {
-      margin: 50px auto;
-      text-align: center;
-    }
-    .container .info h1 {
-      margin: 0 0 15px;
+  body {
+    background: url("<?echo $backgrounds[array_rand($backgrounds)];?>") !important;
+    background-size: cover !important;
+    overflow-y: hidden;
+    width: 100%;
+    height: 100%;
+    background-position: center;
+  }
+  .content {
+      background: transparent;
+      box-shadow: none;
+      height: 100vh;
       padding: 0;
-      font-size: 36px;
-      font-weight: 300;
-      color: #1a1a1a;
-    }
-    .container .info span {
-      color: #4d4d4d;
-      font-size: 12px;
-    }
-    .container .info span a {
-      color: #000000;
-      text-decoration: none;
-    }
-    .container .info span .fa {
-      color: #EF3B3A;
-    }
-    .form input[type=checkbox] {
-        float: none;
-        width: auto;
-        padding: 10px;
-        
-    }
-    .login-page legend {
-        
-        float: left;
-        width: 100%;
-        text-align: center;
-        font-size: 22px;
-        color: #333;
-        margin-bottom: 30px;
-        background: none;
-        margin-top: -25px;
-    }
-    
-    
+  }
+  .header-row {
+    display: none;
+  }
+  .leftcol {
+    display: none;
+  }
+  .content-wrapper {
+      min-height: 900px;
+      z-index: 500;
+      margin-left: 0px;
+      margin-top: 0px;
+      margin-right: 0;
+  }
+  .form-container {
+    position: absolute;
+    top: 150px;
+    left: 50%;
+    width: 300px;
+    margin-left: -150px;
+    height: 378px;
+    padding: 10px;
+    border-radius: 10px;
+    background: #333;
+  }
+  .form-container-inner {
+    border: 3px solid #fff;
+    width: 294px;
+    float: left;
+    background: #fff;
+    height: 372px;
+    border-radius: 10px;
+  }
+  #content .inner {
+    background: none;
+  }
+  .input-field {
+    float: left;
+    width: 284px;
+    padding: 5px;
+    background: none;
+    border-radius: 10px;
+    border: 1px solid #333;
+  }
+  @import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+  .login-page {
+    padding:0;
+    margin: auto;
+    position: absolute;
+    min-height: 100%;
+    display: table;
+    height: 100vh;
+    right: 0;
+  }
+  .form {
+    bottom: 0;
+    overflow: hidden;
+    top: 0;
+    z-index: 1;
+    background: #FFFFFF;
+    width: 360px;
+    padding: 50px;
+    text-align: center;
+    right: 0;
+    height: 100%;
+    display: table-cell;
+    vertical-align: middle;
+  }
+  .form input {
+    font-family: "Roboto", sans-serif;
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+  }
+  .form button {
+    font-family: "Roboto", sans-serif;
+    text-transform: uppercase;
+    outline: 0;
+    background: #4CAF50;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #FFFFFF;
+    font-size: 14px;
+    -webkit-transition: all 0.3 ease;
+    transition: all 0.3 ease;
+    cursor: pointer;
+  }
+  .form button:hover,.form button:active,.form button:focus {
+    background: #43A047;
+  }
+  .form .message {
+    margin: 15px 0 0;
+    color: #b3b3b3;
+    font-size: 12px;
+  }
+  .form .message a {
+    color: #4CAF50;
+    text-decoration: none;
+  }
+  .form .register-form {
+    display: none;
+  }
+  .container {
+    position: relative;
+    z-index: 1;
+    max-width: 300px;
+    margin: 0 auto;
+  }
+  .container:before, .container:after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+  .container .info {
+    margin: 50px auto;
+    text-align: center;
+  }
+  .container .info h1 {
+    margin: 0 0 15px;
+    padding: 0;
+    font-size: 36px;
+    font-weight: 300;
+    color: #1a1a1a;
+  }
+  .container .info span {
+    color: #4d4d4d;
+    font-size: 12px;
+  }
+  .container .info span a {
+    color: #000000;
+    text-decoration: none;
+  }
+  .container .info span .fa {
+    color: #EF3B3A;
+  }
+  .form input[type=checkbox] {
+      float: none;
+      width: auto;
+      padding: 10px;
+  }
+  .login-page legend {
+    float: left;
+    width: 100%;
+    text-align: center;
+    font-size: 22px;
+    color: #333;
+    margin-bottom: 30px;
+    background: none;
+    margin-top: -25px;
+    border-bottom: 0;
+  }
+  .sidebar {
+    display: none;
+  }
+
 </style>
+
+
 
 <div class="login-page">
   <div class="form">
-      
+
       <legend>Sign In</legend>
-    
+
     <form class="login-form validate" method="post" id="login_form" action="/admin?option=login">
         <input type="hidden" name="login" value="1">
       <input type="text" name="email" id="email" placeholder="username"/>
@@ -171,14 +197,14 @@ if( $auth->user['admin'] ){
               <p style="color:red;">Username is required</p>
               <br>
           <? } ?>
-          
+
       <input type="password" name="password" id="password" placeholder="password"/>
       <? if( in_array('password',$auth->errors) ){ ?>
       <p style="color:red;">Password is required</p>
       <br>
       <? } ?>
-      
-      
+
+
       <button>login</button>
       <p class="message"><input type="checkbox" name="remember" value="1" checked="checked"> Remember me?</p>
     </form>
