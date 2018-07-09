df443cbe4f00b10f0fa6d163166d60f0590218e4
Adam Jimenez <adam.jimenez@gmail.com>
2018-07-09 11:26:34 +0100

    list widget

---
+++ _lib/cms/js/ui.list.js
@@ -0,0 +1,63 @@
+$.widget( "custom.list", {
+	// default options
+	options: {
+		url: null,
+		template: null,
+		addButton: null,
+		removeButton: null
+	},
+ 
+	// The constructor
+	_create: function() {
+		var self = this;
+		this.newValueId = 0;
+		
+		if (this.options.addButton) {
+			$('body').on('click', this.options.addButton, function(e) {
+				e.preventDefault();
+				self.addItem($(this).data('target'), $(this).data('template'));
+			});
+		}
+	},
+	addItem: function(data) {
+		this.newValueId--;
+		this.xhr = null;
+		
+		var row = $($(this.options.template).html()).appendTo(this.element);
+		if (data) {
+			for (var key in data) {
+				row.find('.' + key).val(data[key]);
+			};
+		}
+		row.find('[data-remove]').click(function(e) {
+			e.preventDefault();
+			$(e.target).closest('tr').remove();
+		});
+	},
+	addItems: function(data) {
+		var self = this;
+		data.forEach(function(item) {
+			self.addItem(item);
+		});
+	},
+	removeAll: function() {
+		this.element.children().remove();
+	},
+	get: function(data) {
+		var self = this;
+		
+		if (this.xhr) {
+			this.xhr.abort();
+		}
+		
+		return this.xhr = $.ajax({
+			dataType: "json",
+			url: this.options.url,
+			data: data,
+			success: function(data) {
+				self.removeAll();
+				self.addItems(data.items);
+			}
+		});
+	}
+});
