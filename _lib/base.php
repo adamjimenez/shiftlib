60446fcc84ac941d86795b6793ac59d2a22b2334
Adam Jimenez <adam.jimenez@gmail.com>
2019-07-30 10:41:09 +0100

    staging database

--- _lib/base.php
+++ _lib/base.php
@@ -56,14 +56,19 @@ $root_folder = $_SERVER['DOCUMENT_ROOT'];
 chdir($root_folder);
 
 require_once(dirname(__FILE__).'/core/common.php');
-include($root_folder.'/_inc/config.php');
+require($root_folder.'/_inc/config.php');
 
 if( $db_config['user'] or $db_connection ){
 	$cms = new cms;
 
 	if( !$db_connection ){
-		$db_connection = mysqli_connect($db_config['host'], $db_config['user'], $db_config['pass'], $db_config['name']) or trigger_error(mysqli_connect_error(), E_USER_ERROR);
+		if ($_SERVER['LIB_ENV']=='stage') {
+			$db_connection = mysqli_connect($db_config['dev_host'], $db_config['dev_user'], $db_config['dev_pass'], $db_config['dev_name']) or trigger_error(mysqli_connect_error(), E_USER_ERROR);
 		mysqli_set_charset($db_connection, 'utf8');;
+		} else {
+			$db_connection = mysqli_connect($db_config['host'], $db_config['user'], $db_config['pass'], $db_config['name']) or trigger_error(mysqli_connect_error(), E_USER_ERROR);
+		mysqli_set_charset($db_connection, 'utf8');;
+		}
 	}
 
     $auth = new auth();
