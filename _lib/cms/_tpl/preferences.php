64b926ee23dfbdb0397df64c95770ccda0728596
Adam Jimenez <adam.jimenez@gmail.com>
2019-12-18 12:26:39 +0000

    redundant files

--- _lib/cms/_tpl/preferences.php
+++
@@ -1,8 +0,0 @@
-<div class="col-sm-6">
-	<form method="post">
-		<input type="hidden" name="save" value="true" >
-		<h3>Change Your Password:</h3>
-		<input type="password" name="password" class="form-control"><br />
-		<button type="submit" class="btn btn-default save-btn">Save</button>
-	</form>
-</div>
