25afe09a8df2f3cd05d16471609bbe0fc3f1e05c
Ben Squire <b.squire@gmail.com>
2020-01-07 22:54:40 +0000

    Added validator for timestamp

--- _lib/cms/components/timestamp.php
+++ _lib/cms/components/timestamp.php
@@ -7,9 +7,15 @@ class timestamp extends date
     public $field_type = 'hidden';
     public $field_sql = 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP';
 
+    /**
+     * https://stackoverflow.com/questions/2524680/check-whether-the-string-is-a-unix-timestamp
+     *
+     * @param mixed $value
+     * @return bool
+     */
     public function is_valid($value)
     {
-        return Base::is_valid($value);
+        return ((string) (int) $value === $value) && ($value <= PHP_INT_MAX) && ($value >= ~PHP_INT_MAX);
     }
 
     public function format_value($value)
