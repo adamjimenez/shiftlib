c06fbd39e72e38eeecfc34e39b96875dcbdd90fd
Adam Jimenez <adam.jimenez@gmail.com>
2020-01-10 20:31:50 +0000

    use buttons

--- _lib/cms/_tpl/includes/button.php
+++ _lib/cms/_tpl/includes/button.php
@@ -20,7 +20,7 @@ if (false !== $label) {
         ?>
 	<form method="post" <?php if ($button['target']) { ?>target="<?=$button['target'];?>"<?php } ?> style="display:inline" <?php if ($button['confirm']) { ?>onsubmit="return confirm('<?=escape($button['confirm']);?>');"<?php } ?>>
 	<input type="hidden" name="custom_button" value="<?=$k; ?>">
-	    <a class="dropdown-item" href="#" onclick="<?=$button['handler']; ?>" <?=$disabled; ?>><?=$label?></a>
+	    <button class="dropdown-item" href="#" onclick="<?=$button['handler']; ?>" <?=$disabled; ?>><?=$label?></button>
 	</form>
 	<?php
     }
