f2d0bd32ca31e74bced9481109220de0f28d7438
Adam Jimenez <adam.jimenez@gmail.com>
2020-01-10 20:30:33 +0000

    fix missing vars

--- _lib/cms/components/rating.php
+++ _lib/cms/components/rating.php
@@ -18,18 +18,20 @@ class rating extends component
     public function field($field_name, $value = '', $options = [])
     {
         ?>
-        <select name="<?= $field_name; ?>" class="rating" data-section="<?= $this->section; ?>" data-item="<?= $this->content['id']; ?>" <?php if ('avg-rating' == $type) { ?>data-avg='data-avg'<?php } ?> <?= $attribs; ?>>
+        <select name="<?= $field_name; ?>" class="rating" data-section="<?= $this->section; ?>" data-item="<?= $this->content['id']; ?>" <?php if ('avg-rating' == $type) { ?>data-avg='data-avg'<?php } ?> <?= $options['attribs']; ?>>
             <option value="">Choose</option>
             <?= html_options($this->opts['rating'], $value, true); ?>
         </select>
         <?php
     }
 
-    public function value($value)
+    public function value($value, $name)
     {
+        $field_name = underscored($name);
+        
         $value = '<select name="' . $field_name . '" class="rating" disabled="disabled">
             <option value="">Choose</option>
-            ' . html_options($rating_opts, $value, true) . '
+            ' . html_options($this->rating_opts, $value, true) . '
         </select>';
 
         return $value;
