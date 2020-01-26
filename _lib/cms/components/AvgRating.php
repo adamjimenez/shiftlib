2e681cfdf3670369b0cdfbb4d6209308d687d316
Adam Jimenez <adam.jimenez@gmail.com>
2020-01-26 23:42:33 +0000

    var does not exist

--- _lib/cms/components/AvgRating.php
+++ _lib/cms/components/AvgRating.php
@@ -14,7 +14,7 @@ class AvgRating extends Rating implements ComponentInterface
     public function field($field_name, $value = '', $options = []): void
     {
         global $cms; ?>
-        <select name="<?= $field_name; ?>" class="rating" data-section="<?= $cms->section; ?>" data-item="<?= $cms->content['id']; ?>" <?php if ('avg-rating' == $type) { ?>data-avg='data-avg'<?php } ?> <?= $attribs; ?>>
+        <select name="<?= $field_name; ?>" class="rating" data-section="<?= $cms->section; ?>" data-item="<?= $cms->content['id']; ?>" data-avg="data-avg" <?= $options['attribs']; ?>>
             <option value="">Choose</option>
             <?= html_options($this->rating_opts, $value, true); ?>
         </select>
