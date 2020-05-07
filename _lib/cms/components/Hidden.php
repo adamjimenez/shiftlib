0655efa20dc58637925641ab35aefd341972d1a6
Adam Jimenez <adam.jimenez@gmail.com>
2020-05-07 09:40:30 +0100

    hidden fields shown as dates

--- _lib/cms/components/Hidden.php
+++ _lib/cms/components/Hidden.php
@@ -8,18 +8,4 @@ use cms\ComponentInterface;
 class Hidden extends Component implements ComponentInterface
 {
     public $fieldType = 'hidden';
-
-    /**
-     * @param mixed $value
-     * @param string $name
-     * @return string
-     */
-    public function value($value, string $name = ''): string
-    {
-        if ('0000-00-00 00:00:00' !== $value) {
-            $date = explode(' ', $value);
-            $value = dateformat('d/m/Y', $date[0]) . ' ' . $date[1];
-        }
-        return $value;
-    }
 }
