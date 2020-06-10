2fc4455cf790ea5a90b73e41e54a08e786d0525f
Adam Jimenez <adam.jimenez@gmail.com>
2020-06-10 15:04:42 +0100

    blog categories not showing

--- _lib/core/blog.php
+++ _lib/core/blog.php
@@ -47,7 +47,7 @@ class blog
         $this->blog_index = isset($options['blog_index']) ? $options['blog_index'] : array_search('blog', $sections);
 
         $this->table_blog = $options['table_blog'] ?: 'blog';
-        $this->table_categories = $options['table_categories'] ?: 'blog_categories';
+        $this->table_categories = $options['table_categories'] ?: 'blog categories';
 
         $this->category_field = array_search($this->table_categories, $vars['options']);
         
