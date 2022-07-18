029876139c9181793f7e50e669d9471c1070863c
Adam Jimenez <adam.jimenez@gmail.com>
2022-07-18 16:16:01 +0100

    date search

--- _lib/cms/components/Datetime.php
+++ _lib/cms/components/Datetime.php
@@ -61,4 +61,69 @@ class Datetime extends Date implements ComponentInterface
     {
         return Component::isValid($value);
     }
+    
+    /**
+     * @param string $fieldName
+     * @param mixed $value
+     * @param string $func
+     * @param string $tablePrefix
+     * @return string|null
+     */
+    public function conditionsToSql(string $fieldName, $value, $func = '', string $tablePrefix = ''): ?string
+    {
+        if ('now' == $value) {
+            $start = 'NOW()';
+        } elseif ('month' == $func) {
+            $start = dateformat('mY', $value);
+        } else {
+            $start = "'" . escape(dateformat('Y-m-d H:i:s', $value)) . "'";
+        }
+
+        if ($value and is_array($func) and $func['end']) {
+            $end = escape($func['end']);
+
+            $where = '(' . $tablePrefix . $fieldName . ' >= ' . $start . ' AND ' . $tablePrefix . $fieldName . " <= '" . $end . "')";
+        } else {
+            if (!in_array($func, ['=', '!=', '>', '<', '>=', '<='])) {
+                $func = '=';
+            }
+
+            $where = $tablePrefix . $fieldName . ' ' . escape($func) . ' ' . $start;
+        }
+
+        return $where;
+    }
+
+    /**
+     * @param $name
+     * @param mixed $value
+     * @return string
+     */
+    public function searchField(string $name, $value): string
+    {
+        $field_name = underscored($name);
+
+        $html = [];
+        $html[] = '<div>';
+        $html[] = '<label class="col-form-label">' . ucfirst($name) . '</label>';
+        $html[] = '<div>';
+        $html[] = '<div style="float:left">';
+        $html[] = 'From&nbsp;';
+        $html[] = '</div>';
+        $html[] = '<div style="float:left">';
+        $html[] = '<input type="datetime-local" name="' . $field_name . '" value="' . $_GET[$field_name] . '" autocomplete="off" class="form-control">';
+        $html[] = '</div>';
+        $html[] = '<div style="float:left">';
+        $html[] = '&nbsp;To&nbsp;';
+        $html[] = '</div>';
+        $html[] = '<div style="float:left">';
+        $html[] = '<input type="datetime-local" name="func[' . $field_name . '][end]" value="' . $_GET['func'][$field_name]['end'] . '" autocomplete="off" class="form-control">';
+        $html[] = '</div>';
+        $html[] = '<br style="clear: both;">';
+        $html[] = '</div>';
+        $html[] = '</div>';
+        $html[] = '<br>';
+
+        return implode(' ', $html);
+    }
 }
