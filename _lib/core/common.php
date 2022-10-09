f971a9262fd7cb854a4e48513faa2d3b54782a2f
Adam Jimenez <adam.jimenez@gmail.com>
2022-10-09 17:27:45 +0100

    postcode validation

--- _lib/core/common.php
+++ _lib/core/common.php
@@ -1180,7 +1180,7 @@ function is_odd($number): int
 
 function is_postcode($code): bool
 {
-    if (!preg_match('/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/', $code)) {
+    if (strlen($code) > 8 || strlen($code) < 6 || !preg_match('/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$/', $code)) {
         return false;
     }
     
